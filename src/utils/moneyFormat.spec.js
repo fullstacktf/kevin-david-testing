690d7f2fc831cfbdc3fecce953e878ab54921934
kdcr <kevincabrerarodriguez@gmail.com>
2020-10-13 18:25:26 +0100

    moneyFormat fix

--- src/utils/moneyFormat.spec.js
+++ src/utils/moneyFormat.spec.js
@@ -9,7 +9,7 @@ assertEquals(moneyFormat(9500.95), '$9500.95')
 function assertEquals(result, expectedResult) {
     if (result != expectedResult) {
         throw new Error(
-            'This test is failing: Expected "${expectedResult}"'
+            `This test is failing: Expected "${expectedResult}", got "${result}"`,
         )
     }
 }
